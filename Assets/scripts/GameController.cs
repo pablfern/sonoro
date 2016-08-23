73c2921f59b22f0b427d36b095e6284cbbc32c42
Pablo <pablfern@itba.edu.ar>
2016-08-23 20:47:29 -0300

    Added game controller

---
+++ Assets/scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public int lives;
+    public int score;
+    public int level;
+
+    // pool de tiros
+    // pool de Asteroides
+
+    
+	// Use this for initialization
+	void Start () {
+        this.lives = 3;
+        this.score = 0;
+        this.level = 1;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
