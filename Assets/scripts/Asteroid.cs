1b949073947ae50f1f8182e7370ba114ca627abf
Pablo <pablfern@itba.edu.ar>
2016-08-27 11:45:45 -0300

    Added logic to spawn Asteroids

---
+++ Assets/scripts/Asteroid.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+    public float width;
+    public float height;
+    public float rotationSpeed = 2.0f;
+
+    private Rigidbody2D rb;
+    private float xForce;
+    private float yForce;
+
+    // Use this for initialization
+    void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        setPosition();
+        setInitialMovement();
+    }
+
+    void setPosition() {
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+        transform.position = new Vector2( Random.Range(-screenWidth, screenWidth), Random.Range(-screenHeight, screenHeight));
+    }
+
+    void setInitialMovement() {
+        rb.AddTorque(0.5f, ForceMode2D.Impulse);
+        xForce = Random.Range(-4, 4) * (Time.deltaTime + rotationSpeed);
+        yForce = Random.Range(-4, 4) * (Time.deltaTime + rotationSpeed);
+        rb.AddForce(new Vector2(xForce, yForce));
+    }
+
+    void move() {
+        // TODO: Add code to change asteroids direction
+    }
+
+    // Update is called once per frame
+    void Update () {
+        checkBoundaries();
+        move();
+	}
+
+ 
+    void checkBoundaries()
+    {
+
+        Vector3 pos = transform.position;
+        // es 6 en total, va de -3 a 3
+        float verticalSeen = Camera.main.orthographicSize * 2.0f;
+        // es 8 en total, va desde -4 a 4
+        float horizontalSeen = verticalSeen * Screen.width / Screen.height;
+
+        float maxX = horizontalSeen / 2;
+        float minX = maxX * -1;
+        float maxY = verticalSeen / 2;
+        float minY = maxX * -1;
+
+        if (pos.x < minX - width)
+        {
+            transform.position = new Vector3(maxX, pos.y, pos.z);
+        }
+        if (pos.x > maxX + width)
+        {
+            transform.position = new Vector3(minX, pos.y, pos.z);
+        }
+        if (pos.y < minY - height)
+        {
+            transform.position = new Vector3(pos.x, maxY, pos.z);
+        }
+        if (pos.y > maxY + height)
+        {
+            transform.position = new Vector3(pos.x, minY, pos.z);
+        }
+    }
+}
--- Assets/scripts/GameController.cs
+++ Assets/scripts/GameController.cs
@@ -1,25 +1,64 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
     public int lives;
     public int score;
     public int level;
+    public GameObject asteroidPrefab;
+    public int asteroidPoolSize;
+    
+    private float nextActionTime;
+    public float period;
+    private Stack<GameObject> inactiveAsteroidsStack;
 
     // pool de tiros
     // pool de Asteroides
 
-    
-	// Use this for initialization
-	void Start () {
+
+    // Use this for initialization
+    void Start () {
         this.lives = 3;
         this.score = 0;
         this.level = 1;
+        this.nextActionTime = 0.0f;
+        this.period = 1.0f;
+        createAsteroidPool();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void createAsteroidPool() {
+        inactiveAsteroidsStack = new Stack<GameObject>();
+        for (int i = 0; i < this.asteroidPoolSize; i++)
+        {
+            GameObject obj = (GameObject)Instantiate(asteroidPrefab);
+            obj.SetActive(false);
+            inactiveAsteroidsStack.Push(obj);
+        }
+    }
+
+    public GameObject getAsteroid() {
+        if (inactiveAsteroidsStack.Count > 0 ) {
+            return inactiveAsteroidsStack.Pop();
+        }
+        return null;
+    }
+
+    public void returnAsteroid(GameObject asteroid) {
+        inactiveAsteroidsStack.Push(asteroid);
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Time.time > nextActionTime)
+        {
+            nextActionTime += period;
+            GameObject obj = getAsteroid();
+            if (obj != null)
+            {
+                obj.active = true;
+            }
+        }
+    }
 }
