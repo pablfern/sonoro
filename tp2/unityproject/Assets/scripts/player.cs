db00d85276d15b00d7effd50f2597a7fb861fb68
Facundo Alderete <facualderete@gmail.com>
2016-09-23 19:32:42 -0300

    Flippers funcionando un poco mejor

--- tp2/unityproject/Assets/scripts/player.cs
+++ tp2/unityproject/Assets/scripts/player.cs
@@ -5,7 +5,11 @@ public class Player : MonoBehaviour {
 
 	public int initialThrust;
 	private bool isPlaying = false;
-	private Vector3 initialPosition = new Vector3 (22.5f, 1.0f, -38.5f);
+	private Vector3 initialPosition = new Vector3 (3.5f, 1.0f, -32.5f);
+//	private Vector3 initialPosition = new Vector3 (22.5f, 1.0f, -38.5f);
+
+	private float launchPower = 0.0f;
+	private float maxLaunchPower = 2.0f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,12 +18,21 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		// TODO: quitar esto cuando finalice el desarrollo
 		if (Input.GetKey(KeyCode.Return)){
 			resetPosition ();
 		}
-		if (Input.GetKey(KeyCode.Space) && !isPlaying){
-			launch ();
+		if (!isPlaying) {
+			if (Input.GetKey(KeyCode.Space)) {
+				launchPower += Time.deltaTime;
+			}
+			if (Input.GetKeyUp(KeyCode.Space)){
+				launch ();
+			}
 		}
+
+
+
 
 	}
 
@@ -29,8 +42,13 @@ public class Player : MonoBehaviour {
 	}
 
 	void launch () {
+		// TODO: implementar velocidad de lanzamiento variable de acuerdo al tiempo de key down
 		isPlaying = true;
 		Rigidbody rb = gameObject.GetComponent<Rigidbody> ();
-		rb.AddForce(new Vector3 (0, 0, 1) * initialThrust, ForceMode.Impulse);
+		if (launchPower > maxLaunchPower) {
+			launchPower = maxLaunchPower;
+		}
+		rb.AddForce(new Vector3 (0, 0, 1) * initialThrust * launchPower, ForceMode.Impulse);
+		launchPower = 0.0f;
 	}
 }
