cffbb132d035e5d1e62c0a3a5699d3857d1352f9
valeserber <valserber@gmail.com>
2016-09-25 18:18:17 -0300

    adding scores for bumpers

--- tp2/unityproject/Assets/scripts/Bumper.cs
+++ tp2/unityproject/Assets/scripts/Bumper.cs
@@ -7,7 +7,7 @@ public class Bumper : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -19,9 +19,18 @@ public class Bumper : MonoBehaviour {
 		GameObject go = c.gameObject;
 		if (go.CompareTag ("Player")) {
 			Debug.Log ("Collision with bumper");
-			GameController.instance.addScore (100);
+			addScore ();
 			Rigidbody rb = go.GetComponent<Rigidbody> ();
 			rb.AddForce(-c.contacts[0].normal * bumperForce * rb.velocity.magnitude, ForceMode.Impulse);
 		}
 	}
+
+	private void addScore() {
+		if (this.gameObject.CompareTag ("CentralBumper")) {
+			GameController.instance.addScore (100);
+		} else if (this.gameObject.CompareTag ("TriangleBumper")) {
+			GameController.instance.addScore (500);
+		}
+	}
+		
 }
